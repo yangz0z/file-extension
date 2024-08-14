1675d9f5e61385409f2f94f5c60e36f821c28a21
yanghj <gmlwls0522@gmail.com>
2024-08-14 15:31:11 +0900

    vite의 server proxy 설정 vercel 이슈로 axios baseURL 지정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,7 +8,7 @@ import router from './router'
 import axios from 'axios'
 import vueCookies from 'vue-cookies'
 
-// axios.defaults.baseURL = import.meta.env.VITE_API_URL || ''
+axios.defaults.baseURL = process.env.VITE_API_URL || ''
 
 const app = createApp(App)
 
